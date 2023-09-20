bc0449d4b0592760663f9e1f41e2c56b73ce000a
artem.chetvernin <chetverninart@gmail.com>
2023-09-20 23:56:01 +0300

    Initial commit

---
+++ src/test/java/ru/pudge/picker/PickerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pudge.picker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
